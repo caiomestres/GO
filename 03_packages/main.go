9bd2ad20b2ba68597615de11063ccbb3e947dcdb
Caio Mestres <caiodev@gmail.com>
2019-05-19 13:23:26 -0300

    packages

---
+++ 03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/caiomestres/gocrash/03_packages/strutils"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutils.Reverse("Hello"))
+}
